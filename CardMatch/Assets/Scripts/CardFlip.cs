ebcd077a340901259c9b8f2af0695329b1482a2a
Kotob M <realkotob@gmail.com>
2024-11-10 11:19:58 +0200

    Add mobile touch support

--- CardMatch/Assets/Scripts/CardFlip.cs
+++ CardMatch/Assets/Scripts/CardFlip.cs
@@ -22,8 +22,6 @@ public class CardFlip : MonoBehaviour
     private bool isOpen = false;
     private bool isFlipping = false;
 
-  
-
     void Start()
     {
         backRotation = rotationParent.rotation;
@@ -33,16 +31,27 @@ public class CardFlip : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
         {
-            // Debug.Log("MouseButtonDown");
-            if (RectTransformUtility.RectangleContainsScreenPoint(GetComponent<RectTransform>(), Input.mousePosition))
+            if (Input.touchCount > 0)
             {
-                FlipCard();
+                CheckPressed(Input.GetTouch(0).position);
+            }
+            else
+            {
+                CheckPressed(Input.mousePosition);
             }
         }
 
         ExecuteFlipping();
+    }
+
+    private void CheckPressed(Vector2 inputPosition)
+    {
+        if (RectTransformUtility.RectangleContainsScreenPoint(GetComponent<RectTransform>(), inputPosition))
+        {
+            FlipCard();
+        }
     }
 
     private void FlipCard()
@@ -59,23 +68,22 @@ public class CardFlip : MonoBehaviour
             float step = flipSpeed * Time.deltaTime;
             if (isOpen)
             {
-                rotationParent.rotation =
-                    Quaternion.RotateTowards(rotationParent.rotation, frontRotation, step * 180);
+                rotationParent.rotation = Quaternion.RotateTowards(rotationParent.rotation, frontRotation, step * 180);
                 var angle = Quaternion.Angle(rotationParent.rotation, frontRotation);
                 if (angle < 1f)
                 {
                     rotationParent.rotation = frontRotation;
                     isFlipping = false;
                 }
-                if(angle<90){
+                if (angle < 90)
+                {
                     imageFront.enabled = true;
                     imageBack.enabled = false;
                 }
             }
             else
             {
-                rotationParent.rotation =
-                    Quaternion.RotateTowards(rotationParent.rotation, backRotation, step * 180);
+                rotationParent.rotation = Quaternion.RotateTowards(rotationParent.rotation, backRotation, step * 180);
                 var angle = Quaternion.Angle(rotationParent.rotation, backRotation);
                 if (angle < 1f)
                 {
